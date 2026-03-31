1ed3d79874bd9dde1b89a2e4d11e59b0c86bb874
nazyypaaisyputri-source <nazyypaaisyputri@gmail.com>
2026-04-01 00:09:36 +0700

    menambahkan komentar penjelasan di setiap fungsi dan prosedur

--- aduh.cpp
+++ aduh.cpp
@@ -1,19 +1,22 @@
 #include <iostream>
 using namespace std;
 
+// Variabel global
 int angka;
 int pilihan;
 
+// Fungsi cek prima
 bool isPrima(int n) {
+    if (n < 2) return false;
     int i = 2;
     while (i * i <= n) {
         if (n % i == 0) return false;
         i++;
     }
-    if (n < 2) return false;
     return true;
 }
 
+// Fungsi cek fibonacci
 bool isFibonacci(int n) {
     int a = 0, b = 1;
     if (n == a || n == b) return true;
@@ -27,6 +30,7 @@ bool isFibonacci(int n) {
     return false;
 }
 
+// Prosedur tampilkan menu
 void tampilMenu() {
     cout << "\n--- MENU CEK BILANGAN ---" << endl;
     cout << "1. Cek Bilangan Prima" << endl;
@@ -35,17 +39,19 @@ void tampilMenu() {
     cout << "Pilih menu: ";
 }
 
-
+// Prosedur input user
 void inputUser() {
     cout << "Masukkan angka yang mau dicek: ";
     cin >> angka;
 }
 
+// Prosedur output hasil prima
 void tampilkanHasilPrima(bool hasil) {
     if (hasil) cout << "=> " << angka << " adalah bilangan prima!" << endl;
     else cout << "=> " << angka << " bukan bilangan prima." << endl;
 }
 
+// Prosedur output hasil fibonacci
 void tampilkanHasilFibo(bool hasil) {
     if (hasil) cout << "=> " << angka << " termasuk deret Fibonacci." << endl;
     else cout << "=> " << angka << " tidak termasuk deret Fibonacci." << endl;
@@ -57,26 +63,21 @@ int main() {
         cin >> pilihan;
 
         switch (pilihan) {
-        case 1:
-        inputUser();
-        tampilkanHasilPrima(isPrima(angka));
-            break;
-        case 2:
-        inputUser();
-        tampilkanHasilFibo(isFibonacci(angka));
-            break;
-        case 0:
+            case 1:
+                inputUser();
+                tampilkanHasilPrima(isPrima(angka));
+                break;
+            case 2:
+                inputUser();
+                tampilkanHasilFibo(isFibonacci(angka));
+                break;
+            case 0:
                 cout << "Program selesai, terima kasih!" << endl;
                 return 0;
-        default:
+            default:
                 cout << "Pilihan salah!" << endl;
                 break;
-
+        }
     }
-    }
-    return 0;
-}
-
-int main() {
     return 0;
 }
